a333b7c76037d6c9100e0bb645ea16360d139eab
Andrey Lema Molina <andrey_molina91@hotmail.com>
2021-09-12 19:49:02 -0300

    Presentation Project ConsiliacionBancaria

---
+++ Excel-A-Datagrid/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Excel_A_Datagrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVDataImport = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVDataImport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVDataImport
+            // 
+            this.DGVDataImport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVDataImport.Location = new System.Drawing.Point(12, 74);
+            this.DGVDataImport.Name = "DGVDataImport";
+            this.DGVDataImport.Size = new System.Drawing.Size(776, 364);
+            this.DGVDataImport.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DGVDataImport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVDataImport)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVDataImport;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Excel-A-Datagrid/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Excel_A_Datagrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ExcelConnection(string fileName)
+        {
+            return @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+                   @"Data Source=" + fileName + ";" +
+                   @"Extended Properties=" + Convert.ToChar(34).ToString() +
+                   @"Excel 12.0" + Convert.ToChar(34).ToString() + ";";
+        }
+
+        DataView DataImport(string filename)
+        {
+            var excelconnection = ExcelConnection(filename);
+            //string connection = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", filename);
+            OleDbConnection connector = new OleDbConnection(excelconnection);
+            connector.Open();
+            OleDbCommand query = new OleDbCommand("select * from [Hoja1$]", connector);
+            OleDbDataAdapter adapter = new OleDbDataAdapter
+            {
+                SelectCommand = query
+            };
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+            connector.Close();
+            return dataset.Tables[0].DefaultView;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog
+            {
+                Filter = "Excel | *.xls;*xlsx;*.csv;",
+                Title = "Seleccionar Archivo"
+            };
+
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {
+                DGVDataImport.DataSource = DataImport(openfiledialog.FileName);
+            }
+        }
+    }
+}
